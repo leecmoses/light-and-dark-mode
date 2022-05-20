69803cabc1af9ea4ade55cd209e8444592a1e5a0
Moses Lee <leecmoses@gmail.com>
2022-05-20 17:40:16 -0400

    Use localStorage to determine currentTheme

--- script.js
+++ script.js
@@ -36,12 +36,27 @@ const lightMode = () => {
 const switchTheme = (e) => {
   if (e.target.checked) {
     document.documentElement.setAttribute("data-theme", "dark");
+    localStorage.setItem("theme", "dark");
     darkMode();
   } else {
     document.documentElement.setAttribute("data-theme", "light");
+    localStorage.setItem("theme", "light");
     lightMode();
   }
 };
 
 // Event Listener
 toggleSwitch.addEventListener("change", switchTheme);
+
+// Check Local Storage for Theme
+const currentTheme = localStorage.getItem("theme");
+if (currentTheme) {
+  document.documentElement.setAttribute("data-theme", currentTheme);
+
+  if (currentTheme === "dark") {
+    toggleSwitch.checked = true;
+    darkMode();
+  }
+}
+
+console.log(currentTheme);
