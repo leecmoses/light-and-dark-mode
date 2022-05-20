be36f8e2467209f094b6ef25fb938cd1f9e24ef4
Moses Lee <leecmoses@gmail.com>
2022-05-20 17:17:32 -0400

    Add toggle for light and dark mode

---
+++ script.js
@@ -0,0 +1,15 @@
+"use strict";
+
+const toggleSwitch = document.querySelector('input[type="checkbox"]');
+
+// Switch Theme Dynamically
+const switchTheme = (e) => {
+  if (e.target.checked) {
+    document.documentElement.setAttribute("data-theme", "dark");
+  } else {
+    document.documentElement.setAttribute("data-theme", "light");
+  }
+};
+
+// Event Listener
+toggleSwitch.addEventListener("change", switchTheme);
